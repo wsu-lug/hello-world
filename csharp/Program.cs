30798da879779e5535e8344c8d122728e88e09c7
Ramesh Kr Sah <rameshkrsah49@gmail.com>
2018-10-04 16:01:39 -0700

    added hello world for c sharp

---
+++ csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
